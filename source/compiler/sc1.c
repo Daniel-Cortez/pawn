43a88096b521c7774d949493b83340518feb61de
Daniel_Cortez <gromovstanislav@yandex.ru>
2019-08-06 23:31:16 +0700

    __emit: Switch to the code segment when used outside functions

--- source/compiler/sc1.c
+++ source/compiler/sc1.c
@@ -1758,6 +1758,7 @@ static void parse(void)
       /* ignore zero's */
       break;
     case t__EMIT:
+      begcseg();
       emit_flags |= efGLOBAL;
       lex(&val,&str);
       emit_parse_line();
